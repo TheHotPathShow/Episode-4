d8045a014fcb562c2c2688be10a514137157cfb5
Johnny Thompson <jthompson@tmg.dev>
2024-02-16 11:12:12 -0800

    Initial commit of project files.

---
+++ Assets/Scripts/ApplyUnscaledDamageSystem.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+
+namespace THPS.DamageSystem
+{
+    public partial struct ApplyUnscaledDamageSystem : ISystem
+    {
+        public void OnUpdate(ref SystemState state)
+        {
+            foreach (var (currentHitPoints, damageBuffer, team) in SystemAPI
+                         .Query<RefRW<CurrentHitPoints>, DynamicBuffer<DamageBufferElement>, EntityTeam>()
+                         .WithAll<IgnoreDamageMultiplicationTag>()
+                         .WithOptions(EntityQueryOptions.FilterWriteGroup))
+            {
+                foreach (var damageElement in damageBuffer)
+                {
+                    if (damageElement.DamageType == DamageType.None) continue;
+                    if (damageElement.DamageType == DamageType.Healing)
+                    {
+                        if (team.Value != damageElement.DamageTeam) continue;
+                        currentHitPoints.ValueRW.Value += damageElement.HitPoints;
+                        continue;
+                    }
+
+                    if (team.Value == damageElement.DamageTeam) continue;
+                    currentHitPoints.ValueRW.Value -= damageElement.HitPoints;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/QueueDamageSystem.cs
@@ -0,0 +1,104 @@
+﻿using Unity.Entities;
+using UnityEngine.InputSystem;
+
+namespace THPS.DamageSystem
+{
+    // This system would be replaced with multiple systems through your codebase that apply damage, healing, etc.
+    public partial struct QueueDamageSystem : ISystem
+    {
+        private ComponentLookup<DamageProperties> _damagePropertiesLookup;
+
+        public void OnCreate(ref SystemState state)
+        {
+            _damagePropertiesLookup = SystemAPI.GetComponentLookup<DamageProperties>();
+        }
+
+        public void OnUpdate(ref SystemState state)
+        {
+            // var damageEntity = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+            
+            if (Keyboard.current[Key.Digit1].wasPressedThisFrame)
+            {
+                var damageEntity = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(damageEntity, new DamageProperties
+                {
+                    DamageType = DamageType.Physical,
+                    DamageTeam = TeamType.Red,
+                    HitPoints = 10
+                });
+            }
+
+            if (Keyboard.current[Key.Digit2].wasPressedThisFrame)
+            {
+                var damageEntity = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(damageEntity, new DamageProperties
+                {
+                    DamageType = DamageType.Physical,
+                    DamageTeam = TeamType.Red,
+                    HitPoints = 10
+                });
+                
+                var damageEntity2 = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(damageEntity2, new DamageProperties
+                {
+                    DamageType = DamageType.Physical,
+                    DamageTeam = TeamType.Red,
+                    HitPoints = 10
+                });
+            }
+            
+            if (Keyboard.current[Key.Digit3].wasPressedThisFrame)
+            {
+                var damageEntity = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(damageEntity, new DamageProperties
+                {
+                    DamageType = DamageType.Magic,
+                    DamageTeam = TeamType.Red,
+                    HitPoints = 10
+                });
+            }
+            
+            if (Keyboard.current[Key.R].wasPressedThisFrame)
+            {
+                var healingEntityRed = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(healingEntityRed, new DamageProperties
+                {
+                    DamageType = DamageType.Healing,
+                    DamageTeam = TeamType.Red,
+                    HitPoints = 100
+                });
+                
+                var healingEntityBlue = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(healingEntityBlue, new DamageProperties
+                {
+                    DamageType = DamageType.Healing,
+                    DamageTeam = TeamType.Blue,
+                    HitPoints = 100
+                });
+                
+                var healingEntityGreen = state.EntityManager.CreateEntity(typeof(DestroyEntityTag));
+                state.EntityManager.AddComponentData(healingEntityGreen, new DamageProperties
+                {
+                    DamageType = DamageType.Healing,
+                    DamageTeam = TeamType.Green,
+                    HitPoints = 100
+                });
+            }
+            
+            _damagePropertiesLookup.Update(ref state);
+
+            foreach (var curDamageProperties in SystemAPI.Query<DamageProperties>())
+            {
+                foreach (var damageBuffer in SystemAPI.Query<DynamicBuffer<DamageBufferElement>>())
+                {
+                    damageBuffer.Add(new DamageBufferElement
+                    {
+                        DamageType = curDamageProperties.DamageType,
+                        DamageTeam = curDamageProperties.DamageTeam,
+                        HitPoints = curDamageProperties.HitPoints
+                    });
+                }
+            }
+        }
+    }
+}
